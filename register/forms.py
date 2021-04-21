2db65514adc062e7be3e19a5dd01b855308ecb21
roy <roy@ilinkict.com>
2021-04-21 21:45:32 +0800

    change form

--- register/forms.py
+++ register/forms.py
@@ -2,24 +2,26 @@ from django import forms
 from models import *
 from django.core.exceptions import ValidationError
 from string import ascii_letters
+from django.core.validators import MaxLengthValidator
 
 class PatientInformationForm(forms.Form):
-    id = forms.CharField(required=True, attr=[])
-    hosp = forms.CharField(required=True)
-    firstname = forms.CharField(required=True)
-    lastname = forms.CharField(required=True)
-    birthday = forms.DateTimeField(required=False)
-    gender = forms.CharField(required=True)
-    blood = forms.CharField(required=False)
-    tel = forms.CharField(required=False)
-    creatdt = forms.DateTimeField(required=False)
+    id = forms.CharField(required=True, widget=forms.TextInput, validators=[MaxLengthValidator(10, message='[ID] Ensure this value has at most 20 character.')])
+    hosp = forms.CharField(required=True, widget=forms.TextInput, validators=[MaxLengthValidator(10, message='[ID] Ensure this value has at most 20 character.')])
+    firstname = forms.CharField(required=True, widget=forms.TextInput, validators=[MaxLengthValidator(20, message='[ID] Ensure this value has at most 20 character.')])
+    lastname = forms.CharField(required=True, widget=forms.TextInput, validators=[MaxLengthValidator(20, message='[ID] Ensure this value has at most 20 character.')])
+    birthday = forms.DateField(required=False, widget=forms.DateInput)
+    gender = forms.CharField(required=True, widget=forms.Select(choices=[('female', 'Female'),('male', 'Male')]))
+    blood = forms.CharField(required=False, widget=forms.Select(choices=[('A', 'A'), ('B', 'B'), ('AB', 'AB'), ('O', 'O')]))
+    tel = forms.CharField(required=False, widget=forms.TextInput, validators=[MaxLengthValidator(20, message='[ID] Ensure this value has at most 20 character.')])
+    creatdt = forms.DateField(required=False, widget=forms.DateInput)
 
     class Mata:
         model = PatientInformation
         fields = ['id', 'hosp', 'firstname', 'lastname', 'birthday', 'gender', 'blood', 'tel', 'creatdt']
         exclude = ['Seq', 'modifydt']
 
-    def id_format(value):
-        gender = '12'
-        if value[0] not in ascii_letters or len(value) != 10 or value[1:11].isdigit() == False or value[1] not in gender:
-            raise ValidationError('%(value)s is not an even number', params={'value': value})
+    def id_format(self):
+        id = self.cleaned_data['id']
+        if id[0] not in ascii_letters or len(id) != 10 or id[1:11].isdigit() == False:
+            raise ValidationError('%(value)s is not a correct ID number', params={'value': id})
+        return id
